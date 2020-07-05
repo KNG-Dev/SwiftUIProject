a81bb8c97be3f6c9f0764829a0d928d16d53f1d7
Kenny Ho <ken.ho1996@hotmail.com>
2020-07-05 11:55:06 -0400

    Added Menu

---
+++ SwiftUIProject/MenuView.swift
@@ -0,0 +1,22 @@
+//
+//  MenuView.swift
+//  SwiftUIProject
+//
+//  Created by Kenny Ho on 7/5/20.
+//  Copyright © 2020 Kenny Ho. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Account")
+            .font(.system(size: 20, weight: .bold, design: .rounded))
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
