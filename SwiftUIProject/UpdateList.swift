46d993a55737b9bd17bde433fbc46250414c4431
Kenny Ho <ken.ho1996@hotmail.com>
2020-07-05 19:02:50 -0400

    Added Combine framework and enabled editing  list

--- SwiftUIProject/UpdateList.swift
+++ SwiftUIProject/UpdateList.swift
@@ -9,39 +9,57 @@
 import SwiftUI
 
 struct UpdateList: View {
+    @ObservedObject var store = UpdateStore()
+    
+    func addUpdate() {
+        store.updates.append(Update(image: "Card1", title: "New item", text: "New text", date: "Jan 2"))
+    }
+    
     var body: some View {
         NavigationView {
-            List(updateData) { update in
-                NavigationLink(destination: UpdateDetail(update: update)) {
-                    HStack {
-                        Image(update.image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .background(Color.black)
-                            .cornerRadius(20)
-                            .padding(.trailing, 4)
-                        
-                        VStack(alignment: .leading, spacing: 8.0) {
-                            Text(update.title)
-                                .font(.system(size: 20, weight: .bold))
+            List {
+                ForEach(store.updates) { update in
+                    NavigationLink(destination: UpdateDetail(update: update)) {
+                        HStack {
+                            Image(update.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80, height: 80)
+                                .background(Color.black)
+                                .cornerRadius(20)
+                                .padding(.trailing, 4)
                             
-                            Text(update.text)
-                                .lineLimit(2)
-                                .font(.subheadline)
-                            .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
-                            
-                            Text(update.date)
-                                .font(.caption)
-                                .fontWeight(.bold)
-                                .foregroundColor(.secondary)
+                            VStack(alignment: .leading, spacing: 8.0) {
+                                Text(update.title)
+                                    .font(.system(size: 20, weight: .bold))
+                                
+                                Text(update.text)
+                                    .lineLimit(2)
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+                                
+                                Text(update.date)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
                         }
+                        .padding(.vertical, 8)
+                        
                     }
-                    .padding(.vertical, 8)
-                    
+                }
+                .onDelete { index in
+                    self.store.updates.remove(at: index.first!)
+                }
+                
+                .onMove { (source: IndexSet, destination: Int) in
+                    self.store.updates.move(fromOffsets: source, toOffset: destination)
                 }
             }
             .navigationBarTitle(Text("Updates"))
+            .navigationBarItems(leading: Button(action: { self.addUpdate() }) {
+                Text("Add Update")
+            }, trailing: EditButton())
         }
     }
 }
---
+++ SwiftUIProject/UpdateStore.swift
@@ -0,0 +1,14 @@
+//
+//  UpdateStore.swift
+//  SwiftUIProject
+//
+//  Created by Kenny Ho on 7/5/20.
+//  Copyright © 2020 Kenny Ho. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+class UpdateStore: ObservableObject {
+    @Published var updates: [Update] = updateData
+}
