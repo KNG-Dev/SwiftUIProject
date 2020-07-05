dfb5beee99cbd1774a3d61e749fe5631130bdf42
Kenny Ho <ken.ho1996@hotmail.com>
2020-07-05 12:35:14 -0400

    Animated between screens

---
+++ SwiftUIProject/Home.swift
@@ -0,0 +1,57 @@
+//
+//  Home.swift
+//  SwiftUIProject
+//
+//  Created by Kenny Ho on 7/5/20.
+//  Copyright © 2020 Kenny Ho. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var showProfile = false
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Text("Watching")
+                        .font(.system(size: 28, weight: .bold))
+                    Spacer()
+                    Button(action: {self.showProfile.toggle()}) {
+                        Image("Avatar").renderingMode(.original)
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                            .clipShape(Circle())
+                    }
+                }
+                    
+                .padding(.horizontal)
+                .padding(.top, 30)
+                Spacer()
+            }
+            
+            .padding(.top, 44)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 :0), axis: (x: 10.0, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
+            
+            MenuView()
+                .offset(y: showProfile ? 0 : 600)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
